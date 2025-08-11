941fb9a6dc7982d233ae37e3f63f3c9a14200e95
Germán Cáceres <gpcaceres@espe.edu.ec>
2025-08-10 19:35:50 -0500

    Creación de función esPalindromo

---
+++ esPalindromo.js
@@ -0,0 +1,12 @@
+function esPalindromo(texto) {
+  if (typeof texto !== 'string') {
+    throw new TypeError('El argumento debe ser una cadena');
+  }
+  
+  const limpio = texto.toLowerCase().replace(/[\W_]/g, '');
+  const invertido = limpio.split('').reverse().join('');
+  
+  return limpio === invertido;
+}
+
+module.exports = esPalindromo;
